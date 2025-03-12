e2ef25e2c6c028ecd04c9f83a1dc3e47d1bd9bc8
JananySounthy <123716135+JananySounthy@users.noreply.github.com>
2025-03-12 10:51:35 +0530

    Add the Details.java File

---
+++ Details.java
@@ -0,0 +1,14 @@
+class Details{
+	public static void main(String args[]){
+		String Name,Address,DOB,Sex;
+		int Age;
+		//Name=args[0];
+		//Address=args[1];
+		//DOB=args[2];
+		//Sex=args[3];
+		//Age=Integer.parseInt(args[4]);
+		//System.out.println("Name : "+Name+"\n"+"Address : "+Address+"\n"+"Date of Birth : "+DOB+"\n"+"Sex : "+Sex+"\n"+"Age : "+Age);
+	    System.out.println("Name : "+args[0]+"\n"+"Address : "+args[1]+"\n"+"Date of Birth : "+args[2]+"\n"+"Sex : "+args[3]+"\n"+"Age : "+args[4]);
+	
+	}
+}
